ee9033c6d58829088fb9a3fe6fac663e41fe67b8
woha13 <woha13@gmail.com>
2016-07-19 14:43:03 +0300

    Add files via upload

---
+++ Folder synchronizer/FolderSynchronizer/FileSynchronizer/Form1.cs
@@ -0,0 +1,82 @@
+﻿using LogicofCMP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using LogicofCMP;
+
+namespace FolderSynchronizer
+{
+    public partial class FolderSynchronizerForm : Form
+    {
+        public FolderSynchronizerForm()
+        {
+            InitializeComponent();
+
+            FolderPathLeftDialog.SelectedPath = @"d:\Slava\1";
+            FolderPathRightDialog.SelectedPath = @"d:\Slava\2";
+            textBoxFolderPathLeft.Text = @"d:\Slava\1";
+            textBoxFolderPathRight.Text = @"d:\Slava\2";
+            textBoxFileMask.Text = @"*.*";
+        }
+        
+        FolderBrowserDialog FolderPathLeftDialog = new FolderBrowserDialog(); //creating dialog window instance for left file path
+        FolderBrowserDialog FolderPathRightDialog = new FolderBrowserDialog(); //creating dialog window instance for right file path
+        ListsofFiles listsofFiles = new ListsofFiles();
+
+
+        private void buttonFolderPathLeft_Click(object sender, EventArgs e)
+        {
+            
+            FolderPathLeftDialog.Description = "Select folder for synchronizaton";
+
+            // checking if user enter folder path manually then in folder browser dialog window this folder will be selected
+            if (textBoxFolderPathLeft.Text != null)
+            {
+                FolderPathLeftDialog.SelectedPath = textBoxFolderPathLeft.Text;
+            }
+
+            
+            if (FolderPathLeftDialog.ShowDialog() == DialogResult.OK) //checking if dialog window closed by clicking OK button
+            {
+                textBoxFolderPathLeft.Text = FolderPathLeftDialog.SelectedPath; //sending selected path into texbox
+            }
+        }
+
+        private void buttonFolderPathRight_Click(object sender, EventArgs e)
+        {
+            FolderPathRightDialog.Description = "Select folder for synchronizaton";
+
+            if (textBoxFolderPathRight.Text != null)
+            {
+                FolderPathRightDialog.SelectedPath = textBoxFolderPathRight.Text;
+            }
+
+
+            if (FolderPathRightDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolderPathRight.Text = FolderPathRightDialog.SelectedPath;
+            }
+        }
+
+        private void FolderSynchronizerForm_Load(object sender, EventArgs e)
+        {
+            listsofFiles.FillListsFromPath(textBoxFolderPathLeft.Text, textBoxFolderPathRight.Text);
+            foreach (FileInfoWoha FIW in listsofFiles.LeftListofFiles)
+            {
+                listViewLeftListofFiles.Items.Add(FIW.Name);
+            }
+            foreach (FileInfoWoha FIW in listsofFiles.RightListofFiles)
+            {
+                listViewRightListofFiles.Items.Add(FIW.Name);
+            }
+        }
+
+
+    }
+}
---
+++ Folder synchronizer/FolderSynchronizer/LogicofCMP/FileInfoWoha.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LogicofCMP
+{
+    public class FileInfoWoha
+    {
+        public string Path;
+        public string Name;
+        public Int64 Size;
+        public DateTime DateCreation;
+        public DateTime DateMoficication;
+        public FileInfoWoha()
+        {
+            Path = "";
+            Name = "";
+            Size = 0;
+            DateCreation = DateTime.MinValue;
+            DateMoficication = DateTime.MinValue;
+        }
+    }
+
+    public class ListsofFiles
+    {
+        public List<FileInfoWoha> LeftListofFiles;
+        public List<FileInfoWoha> RightListofFiles;
+        public List<int> WhatToDo;
+        //1 - Right
+        //2 - Left
+        //3 - Equal
+        //4 - NotEqual
+        //5 - DoesNotExist
+        const int RightIcon = 1;
+        const int LeftIcon = 2;
+        const int EqualIcon = 3;
+        const int NotEqualIcon = 4;
+        const int DNEIcon = 5;
+        public ListsofFiles()
+        {
+            LeftListofFiles = new List<FileInfoWoha>();
+            RightListofFiles = new List<FileInfoWoha>();
+            WhatToDo = new List<int>();
+        }
+
+        public void FillListsFromPath(string SourcePath, string TargetPath)
+        {
+            FillSourcePathList(SourcePath);
+            FillTargetPathList(TargetPath);
+        }
+
+        //private void FillSourcePathList(string SourcePath)
+        //{
+        //    // Process the list of files found in the directory.
+        //    FileInfoWoha fileData = new FileInfoWoha();
+            
+        //    string[] fileEntries = Directory.GetFiles(SourcePath);
+        //    foreach (string fileName in fileEntries)
+        //    {
+        //        //ProcessFile(fileName);
+        //        fileData.Name = fileName;
+        //        LeftListofFiles.Add(fileData);
+        //    }
+                
+
+        //    // Recurse into subdirectories of this directory.
+        //    string[] subdirectoryEntries = Directory.GetDirectories(SourcePath);
+        //    foreach (string subdirectory in subdirectoryEntries)
+        //        FillSourcePathList(subdirectory);
+        //}
+
+        private void FillSourcePathList(string SourcePath)
+        {
+            // Process the list of files found in the directory.
+            
+
+            string[] fileEntries = Directory.GetFiles(SourcePath);
+            foreach (string fileName in fileEntries)
+            {
+                FileInfoWoha fileData = new FileInfoWoha();
+                fileData.Name = fileName;
+                LeftListofFiles.Add(fileData);
+            }
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(SourcePath);
+            foreach (string subdirectory in subdirectoryEntries)
+                FillSourcePathList(subdirectory);
+        }
+
+        private void FillTargetPathList(string TargetPath)
+        {
+            // Process the list of files found in the directory.
+            
+
+            string[] fileEntries = Directory.GetFiles(TargetPath);
+            foreach (string fileName in fileEntries)
+            {
+                FileInfoWoha fileData = new FileInfoWoha();
+                fileData.Name = fileName;
+                RightListofFiles.Add(fileData);
+            }
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(TargetPath);
+            foreach (string subdirectory in subdirectoryEntries)
+                FillTargetPathList(subdirectory);
+        }
+    }
+
+}

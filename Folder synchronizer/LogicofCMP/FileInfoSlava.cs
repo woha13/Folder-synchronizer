d654ce0130edcd9f8be6adee611138d616191962
slavikcx <slavikcx@gmail.com>
2016-07-19 15:34:40 +0300

    Add files via upload
    samalet

---
+++ Folder synchronizer/LogicofCMP/FileInfoSlava.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/// <summary>
+/// SAMALET
+/// </summary>
+namespace LogicofCMP
+{
+    public class Synchronization
+    {
+
+        public void Synchronize (ListsofFiles ListsOfFiles, bool isAsymmetricChecked, bool isByContentChecked)
+          {
+            if (isAsymmetricChecked)
+            {
+                FileInfoWoha TargetListFile = ListsOfFiles.RightListofFiles.First();
+
+                foreach (FileInfoWoha FIW in ListsOfFiles.LeftListofFiles)
+                {
+                    
+                    //!!!Path field returns not only path but also filename
+
+                    //string sourceFile = System.IO.Path.Combine(FIW.Path, FIW.Name);
+                    //string destFile = System.IO.Path.Combine(TargetListFile.Path, FIW.Name);
+
+                    try
+                    {
+                       // System.IO.File.Copy(sourceFile, destFile, true);
+                    }
+                    catch (IOException copyError)
+                    {
+
+                    }
+
+                }
+
+
+            }
+        }
+
+    }
+}

3d7b39c76ec8cf66332cef4f14d0087fe4a9ceef
Woha13 <woha13@gmail.com>
2016-07-28 15:51:54 +0300

    Крайній перед захистом

--- Folder synchronizer/FileSynchronizer/Form1.Designer.cs
+++ Folder synchronizer/FileSynchronizer/Form1.Designer.cs
@@ -357,7 +357,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(949, 476);
+            this.ClientSize = new System.Drawing.Size(949, 477);
             this.Controls.Add(this.buttonDown);
             this.Controls.Add(this.buttonUp);
             this.Controls.Add(this.groupBox2);
